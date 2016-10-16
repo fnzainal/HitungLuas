112e1db756fba6fccb78affa9fb3a7be6e84ab06
Zainal Fahrudin <fnzainal@gmail.com>
2016-10-16 12:21:49 +0700

    update fix bug

--- app/src/main/java/id/zainalfahrudin/hitungluas/MainActivity.java
+++ app/src/main/java/id/zainalfahrudin/hitungluas/MainActivity.java
@@ -2,10 +2,12 @@ package id.zainalfahrudin.hitungluas;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,12 +35,19 @@ public class MainActivity extends AppCompatActivity {
                 if (panjang.length()<1){
                     edtPanjang.setError("tidak boleh kosong");
                     txtLuas.setText("");
+                }else if (!isDouble(panjang)){
+                    edtPanjang.setError("inputan harus angka");
+                    txtLuas.setText("");
                 }else if (lebar.length()<1){
                     edtPanjang.setError(null);
                     edtLebar.setError("tidak boleh kosong");
                     txtLuas.setText("");
+                }else if (!isDouble(lebar)){
+                    edtLebar.setError("inputan harus angka");
+                    txtLuas.setText("");
                 }else{
                     edtLebar.setError(null);
+                    edtPanjang.setError(null);
                     double p = Double.parseDouble(panjang);
                     double l = Double.parseDouble(lebar);
 
@@ -47,8 +56,17 @@ public class MainActivity extends AppCompatActivity {
                     txtLuas.setText("Luas : "+luas);
 
                 }
-
             }
         });
+    }
+
+
+    boolean isDouble(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
